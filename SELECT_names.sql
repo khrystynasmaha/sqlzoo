4f1375cc4caca6cd96e9d7eae8746051b128cbbd
Pythonista § <50423210+khrystynasmaha@users.noreply.github.com>
2019-11-06 21:28:13 -0800

    fixing the error

--- SELECT_names.sql
+++ SELECT_names.sql
@@ -74,6 +74,6 @@ AND capital<>name;
 
 --15. Show the name and the extension where the capital is an extension of name of the country.
 
-SELECT capital, name FROM world
+SELECT capital, REPLACE(capital, name,'')
 WHERE capital LIKE CONCAT(name, '%') 
 AND capital<>name;
