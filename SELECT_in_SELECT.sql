0dc48ad7dcdb7eb403ba1397835d08875b58459d
Pythonista § <50423210+khrystynasmaha@users.noreply.github.com>
2019-11-07 22:21:12 -0800

    Create SELECT_in_SELECT.sql

---
+++ SELECT_in_SELECT.sql
@@ -0,0 +1,10 @@
+--Table: world(name, continent, area, population, gdp)
+
+--1. List each country name where the population is larger than that of 'Russia'.
+
+SELECT name FROM world 
+WHERE population >
+  (SELECT population FROM world 
+    WHERE name = 'Russia');
+    
+--2. 
