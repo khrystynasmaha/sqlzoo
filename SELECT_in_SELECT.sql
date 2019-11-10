ee494150ec0f0045b42809c10649d3c9d1dc9a1d
Pythonista § <50423210+khrystynasmaha@users.noreply.github.com>
2019-11-09 21:26:06 -0800

    adding new exercises with solutions #2-5

--- SELECT_in_SELECT.sql
+++ SELECT_in_SELECT.sql
@@ -7,4 +7,37 @@ WHERE population >
   (SELECT population FROM world 
     WHERE name = 'Russia');
     
---2. 
+--2. Show the names of countries in Europe with a per capita GDP greater than 'United Kingdom'.
+
+SELECT name FROM world 
+WHERE continent ='Europe' AND 
+  gdp/population>
+    (SELECT gdp/population AS per_capita_gdp 
+      FROM world WHERE name= 'United Kingdom');
+    
+--3. List the name and continent of countries in the continents containing either Argentina 
+--or Australia. Order by name of the country.
+
+SELECT name, continent FROM world 
+WHERE continent IN
+  (SELECT continent FROM world 
+    WHERE name ='Argentina' OR name ='Australia')
+ORDER BY name;
+
+--4. Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+
+SELECT name, population FROM world 
+WHERE population>
+  (SELECT population FROM world 
+   WHERE name ='Canada')AND 
+      population<
+  (SELECT population FROM world 
+   WHERE name ='Poland');
+   
+--5. Germany (population 80 million) has the largest population of the countries in Europe. 
+--Austria (population 8.5 million) has 11% of the population of Germany.
+
+SELECT name, population, 
+	CONCAT(ROUND(population/(SELECT population FROM world WHERE name ='Germany')*100, 0),'%')
+FROM world
+WHERE continent ='Europe';
